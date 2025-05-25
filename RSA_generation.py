60be049319fd57db5a3ba8b3c4806ecae5f6762e
Julian Wasylka 1240261 <1240261@isep.ipp.pt>
2025-05-25 17:08:57 +0200

    Added a progress bar to the key generation

--- RSA_generation.py
+++ RSA_generation.py
@@ -2,27 +2,60 @@ from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
+import sys
+import time
+import threading
+import os
 
 KEY_SIZE = 4096
 CIPHER_MODE = AES.MODE_EAX
 IV = get_random_bytes(16) # initial vector, to randomize
 
+stop_progress_bar = threading.Event()
+
+def progress_bar_animation(total_length=50):
+    sys.stdout.write("Generating RSA keys: [")
+    sys.stdout.flush()
+    
+    current_progress = 0
+    while not stop_progress_bar.is_set():
+        if current_progress < total_length:
+            sys.stdout.write("#")
+            sys.stdout.flush()
+            current_progress += 1
+        time.sleep(0.6) 
+    
+    while current_progress < total_length:
+        sys.stdout.write("#")
+        sys.stdout.flush()
+        current_progress += 1
+    
+    sys.stdout.write("]\n")
+    sys.stdout.flush()
+
 def create_rsa_keys():
     pin = input("Enter PIN: ").encode()
     aes_key = SHA256.new(pin).digest()
 
-    rsa_key = RSA.generate(bits=KEY_SIZE)
+    progress_thread = threading.Thread(target=progress_bar_animation)
+    progress_thread.start()
 
-    private_key = rsa_key.export_key(format='PEM')
-    public_key = rsa_key.publickey().export_key(format='PEM')
+    try:
+        rsa_key = RSA.generate(bits=KEY_SIZE)
 
-    cipher = AES.new(aes_key, CIPHER_MODE)
-    encrypted_private_key = cipher.encrypt(private_key)
+        private_key = rsa_key.export_key(format='PEM')
+        public_key = rsa_key.publickey().export_key(format='PEM')
 
-    with open("keys/rsa_private.bin", "wb") as f:
+        cipher = AES.new(aes_key, CIPHER_MODE)
+        encrypted_private_key = cipher.encrypt(private_key)
+    finally:
+        stop_progress_bar.set()
+        progress_thread.join()
+
+    with open("projekt/keys/rsa_private.bin", "wb") as f:
         f.write(encrypted_private_key)
 
-    with open("keys/rsa_public.pem", "wb") as f:
+    with open("projekt/keys/rsa_public.pem", "wb") as f:
         f.write(public_key)
 
     print("Keys saved to files in the keys folder.")
